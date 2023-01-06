1948727d8621cebed7b5bfd3f07709920b8389e2
Mayurimk98 <mayurimkatwe@gmail.com>
2023-01-06 10:05:37 +0530

    9:30am

---
+++ script.js
@@ -0,0 +1,30 @@
+//typescript
+
+//superset => javascript
+
+//code typescript => javascript  => web
+
+//typescript => datatype
+
+
+//to do add of two number
+
+// function add(x, y) {
+//     console.log(x + y) //"20"+10 =>
+// }
+// add(10,20) //30
+
+// add('20',10) //2010
+
+//javscript
+
+// function add(x,y){
+//     if(typeof x == "number" && typeof y == "number"){
+//         console.log(x+y)
+//     }
+//     else{
+//         console.log('plez ener correct number')
+//     }
+// }
+// add('10',2)
+// add(2,3)
